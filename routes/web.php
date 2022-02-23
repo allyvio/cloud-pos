6b57f6a0dface3d5cd64a7ae39a75736bee6cba3
ravlyseven <ravlyseven@gmail.com>
2022-02-23 10:52:06 +0700

    testing 2

--- routes/web.php
+++ routes/web.php
@@ -92,4 +92,3 @@ Route::group(['middleware' => ['auth']], function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/home2', 'HomeController@index')->name('home');
